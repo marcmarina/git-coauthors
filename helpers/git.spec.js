1dad195f8507397163659901f6ae2ccaf292b0c7
Marc Marina <mmarina@findmypast.com>
2022-07-01 14:18:08 +0200

    Refactored simple-git mocking

--- helpers/git.spec.js
+++ helpers/git.spec.js
@@ -1,7 +1,17 @@
 import { getAuthors } from './git';
 
+const mockedLog = jest.fn();
+
 const SAMPLE_LOG = {
   all: [
+    {
+      author_name: 'Kaladin Stormblessed',
+      author_email: 'kaladin@stormblessed.com',
+    },
+    {
+      author_name: 'Hoid',
+      author_email: 'hoid@cosmere.com',
+    },
     {
       author_name: 'Kaladin Stormblessed',
       author_email: 'kaladin@stormblessed.com',
@@ -14,12 +24,16 @@ const SAMPLE_LOG = {
       author_name: 'Hoid',
       author_email: 'hoid@cosmere.com',
     },
+    {
+      author_name: 'Hoid',
+      author_email: 'hoid@cosmere.com',
+    },
   ],
 };
 
 jest.mock('simple-git', () => {
   return () => ({
-    log: () => SAMPLE_LOG,
+    log: () => mockedLog(),
   });
 });
 
@@ -29,6 +43,8 @@ describe('getAuthors', () => {
   });
 
   it('it should return a list of sorted unique authors', async () => {
+    mockedLog.mockResolvedValue(SAMPLE_LOG);
+
     await expect(getAuthors()).resolves.toStrictEqual([
       'Dalinar Kholin <dalinar@kholin.com>',
       'Hoid <hoid@cosmere.com>',
