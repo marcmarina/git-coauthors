69f3c4face473f9fe6fdd795cf459d41e077fe46
Marc Marina <mmarina@findmypast.com>
2022-12-26 19:53:42 +0100

    Expanded command experience

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,9 @@ program.version(config.version).description('Git co-author picker');
 
 program
   .command('pick-authors', { isDefault: true })
-  .description('Pick co-authors from a list')
+  .description(
+    'Pick co-authors from a list and add them to your clipboard (e.g. Co-authored-by: John Doe <jdoe@example.com>).',
+  )
   .action(pickAuthors);
 
 program.parse();
