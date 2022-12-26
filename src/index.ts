129b6a64c9a1b0ec79d5f0513174ea94e32aec88
Marc Marina <marc.marina.miravitlles@gmail.com>
2022-12-27 00:15:17 +0100

    Added options (#61)
    * Added output authors option
    
    * Fixed config module so the build step respects the correct file structure
    
    * Removed subcommand

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,22 @@ import { program } from 'commander';
 import pickAuthors from './commands/pick-authors';
 import config from './helpers/config';
 
-program.version(config.version).description('Git co-author picker');
-
 program
-  .command('pick-authors', { isDefault: true })
+  .version(config.version)
   .description(
     'Pick co-authors from a list and add them to your clipboard (e.g. Co-authored-by: John Doe <jdoe@example.com>).',
   )
-  .action(pickAuthors);
+  .addHelpCommand()
+  .option(
+    '-o, --output-authors',
+    'Output the chosen authors to the console',
+    false,
+  );
 
-program.parse();
+program.parse(process.argv);
+
+const options = program.opts();
+
+pickAuthors({
+  outputAuthors: options.outputAuthors,
+});
