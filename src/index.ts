ad07f8c02fe7d389ad3fb40375cfdbc37ac1375d
Marc Marina <mmarina@findmypast.com>
2022-12-28 19:35:43 +0100

    Brought back subcommand

--- src/index.ts
+++ src/index.ts
@@ -4,20 +4,15 @@ import { program } from 'commander';
 import pickAuthors from './commands/pick-authors';
 import config from './helpers/config';
 
+program.version(config.version).description('Git co-author picker.');
+
 program
-  .version(config.version)
+  .command('pick', { isDefault: true })
   .description(
     'Pick co-authors from a list and add them to your clipboard (e.g. Co-authored-by: John Doe <jdoe@example.com>).',
   )
-  .addHelpCommand()
   .option('-p, --print', 'Print the chosen authors to the console', false)
-  .option('-s, --sort', 'Sort the authors alphabetically', false);
+  .option('-s, --sort', 'Sort the authors alphabetically', false)
+  .action(pickAuthors);
 
 program.parse(process.argv);
-
-const options = program.opts();
-
-pickAuthors({
-  print: options.print,
-  sort: options.sort,
-});
