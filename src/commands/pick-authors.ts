87adf3153ee104cab1764297c51f3b2460ab1647
Marc Marina <mmarina@findmypast.com>
2023-01-02 15:32:54 +0100

    Improved error handling

--- src/commands/pick-authors.ts
+++ src/commands/pick-authors.ts
@@ -16,41 +16,41 @@ const pickAuthorsOptionsSchema = z.object({
 type Options = z.infer<typeof pickAuthorsOptionsSchema>;
 
 export default async function pickAuthors(options: Options): Promise<void> {
-  await assertDirIsRepo();
-
-  const {
-    print,
-    sort,
-    order,
-    limit,
-    recents: recentsFilename,
-  } = pickAuthorsOptionsSchema.parse(options);
-
-  const recentAuthorStore = new JSONStore<Author[]>(recentsFilename, []);
-
-  const recents = await recentAuthorStore.get();
-
-  const authors = await getAuthors({ sort, order, recents, limit });
-
-  const chosen = await checkboxPrompt(authors, {
-    message: 'Which co-authors do you want to select?',
-    toChoice: (author) => ({
-      title: `${author.name} <${author.email}>`,
-      value: author,
-    }),
-  });
-
-  if (!chosen?.length) return;
-
-  await recentAuthorStore.store(combineUnique(chosen, recents));
-
-  const formattedAuthors = chosen.map(toCoauthor).join('\n');
-
-  if (print) {
-    console.log(formattedAuthors);
-  }
-
   try {
+    await assertDirIsRepo();
+
+    const {
+      print,
+      sort,
+      order,
+      limit,
+      recents: recentsFilename,
+    } = pickAuthorsOptionsSchema.parse(options);
+
+    const recentAuthorStore = new JSONStore<Author[]>(recentsFilename, []);
+
+    const recents = await recentAuthorStore.get();
+
+    const authors = await getAuthors({ sort, order, recents, limit });
+
+    const chosen = await checkboxPrompt(authors, {
+      message: 'Which co-authors do you want to select?',
+      toChoice: (author) => ({
+        title: `${author.name} <${author.email}>`,
+        value: author,
+      }),
+    });
+
+    if (!chosen?.length) return;
+
+    await recentAuthorStore.store(combineUnique(chosen, recents));
+
+    const formattedAuthors = chosen.map(toCoauthor).join('\n');
+
+    if (print) {
+      console.log(formattedAuthors);
+    }
+
     await clipboardy.write('\n' + formattedAuthors);
   } catch (err) {
     console.log(err);
--- src/storage/json-store.ts
+++ src/storage/json-store.ts
@@ -10,19 +10,50 @@ export default class JSONStore<T> {
     this.defaultValue = defaultValue;
   }
 
+  async delete(): Promise<void> {
+    try {
+      await fs.unlink(this.filepath);
+    } catch (error) {
+      console.log(`Error while deleting ${this.filepath} file: ${error}`);
+    }
+  }
+
   async get(): Promise<T> {
     try {
+      const fileExists = await this.doesFileExist();
+
+      if (!fileExists) {
+        return this.defaultValue;
+      }
+
       const fileData = await fs.readFile(this.filepath);
 
       return JSON.parse(fileData.toString());
     } catch (error) {
+      console.log(
+        `Error while reading data in ${this.filepath}: ${error}. Returning default value.`,
+      );
       return this.defaultValue;
     }
   }
 
-  async store(data: T): Promise<void> {
-    const dataString = JSON.stringify(data);
+  private async doesFileExist(): Promise<boolean> {
+    try {
+      await fs.access(this.filepath);
 
-    await fs.writeFile(this.filepath, dataString);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async store(data: T): Promise<void> {
+    try {
+      const dataString = JSON.stringify(data);
+
+      await fs.writeFile(this.filepath, dataString);
+    } catch (error) {
+      console.log(`Error while storing data in ${this.filepath}: ${error}`);
+    }
   }
 }
