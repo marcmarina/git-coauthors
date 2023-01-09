c11130dd2fe027aaa4bb644bb538f7caf76bfe02
Marc Marina <mmarina@findmypast.com>
2023-01-09 14:10:07 +0000

    Made storage dir constant uppercase

--- src/storage/recents-storage-helpers.ts
+++ src/storage/recents-storage-helpers.ts
@@ -4,22 +4,22 @@ import path from 'path';
 
 import { currentDirName, doesFileOrDirExist } from '../utils';
 
-const storageDir = path.join(os.homedir(), `.git-coauthors`);
+const STORAGE_DIR = path.join(os.homedir(), `.git-coauthors`);
 
 /**
  * @returns The path to the file where the co-authors are stored for the current working directory.
  */
 export function getAuthorsFilePath(): string {
-  return path.join(storageDir, `${currentDirName()}.json`);
+  return path.join(STORAGE_DIR, `${currentDirName()}.json`);
 }
 
 /**
  * Function that creates the storage directory if it doesn't exist.
  */
 export async function initialiseStorage(): Promise<void> {
-  const doesStorageDirExist = await doesFileOrDirExist(storageDir);
+  const doesStorageDirExist = await doesFileOrDirExist(STORAGE_DIR);
 
   if (!doesStorageDirExist) {
-    await fs.mkdir(storageDir);
+    await fs.mkdir(STORAGE_DIR);
   }
 }
