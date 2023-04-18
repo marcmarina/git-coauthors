8216fc0669c3244df067e2185a9369ac0f485eb9
Marc Marina <mmarina@findmypast.com>
2023-04-18 19:53:22 +0100

    Working tests

--- packages/jest-config/index.js
+++ packages/jest-config/index.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   clearMocks: true,
-  modulePathIgnorePatterns: ['<rootDir>/dist/'],
+  testMatch: ['<rootDir>/src/**/*.spec.ts'],
 };
